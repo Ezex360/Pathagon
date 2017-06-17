a76b8237e586da9dbfcfbdac453119592772dfb6
Ezex360 <eigiachero@gmail.com>
2017-06-17 03:46:13 -0300

    tablero mejorado

--- untitled folder/TictactoeState.java
+++ untitled folder/TictactoeState.java
@@ -69,17 +69,19 @@ public class TictactoeState implements AdversarySearchState, Serializable {
 	} 
 	
 	public String toString() {
-		String print = "";
+		String print = "\n--------------\n|";
 		for (int i=0 ;i<3 ;i++ ) {
             for (int j=0 ;j<3 ;j++ ) {
                 if(board[i][j]==0)
-                    print +=" 0 ";
+                    print +=" 0 |";
                 if(board[i][j]==1)
-                    print +=" X ";
+                    print +=" X |";
                 if(board[i][j]==-1)
-                    print +="   ";
+                    print +="   |";
             }
-            print += "\n";
+            print += "\n--------------\n";
+            if(i!=2)
+                print += "|";
         }
         return print;
 		
